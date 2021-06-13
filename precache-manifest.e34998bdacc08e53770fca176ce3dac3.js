b1f36ae9a36efc05a877fb8c4b1efdabe119078a
Monika Osenkowska <m.osenkowska1@gmail.com>
2021-06-14 00:25:28 +0200

    Updates

---
+++ precache-manifest.e34998bdacc08e53770fca176ce3dac3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f1bf502f53db4c2092",
+    "url": "/Memes_Ranking/static/css/main.05c2bc98.chunk.css"
+  },
+  {
+    "revision": "25f1bf502f53db4c2092",
+    "url": "/Memes_Ranking/static/js/main.2442eeec.chunk.js"
+  },
+  {
+    "revision": "5a8f8649ba3548256540",
+    "url": "/Memes_Ranking/static/js/runtime~main.a798fcdd.js"
+  },
+  {
+    "revision": "da940664d6daff497c68",
+    "url": "/Memes_Ranking/static/js/2.baafa6a8.chunk.js"
+  },
+  {
+    "revision": "e13bfb62faeb205288bb35a062da66ee",
+    "url": "/Memes_Ranking/static/media/logo.e13bfb62.png"
+  },
+  {
+    "revision": "c3c2f3424112ba267f2f0c37ea6a03b7",
+    "url": "/Memes_Ranking/static/media/meme_1.c3c2f342.png"
+  },
+  {
+    "revision": "9daef0fecd46a81839b813776e12761c",
+    "url": "/Memes_Ranking/static/media/meme_2.9daef0fe.jpg"
+  },
+  {
+    "revision": "a2357457741d84ea3fb2602883db5b92",
+    "url": "/Memes_Ranking/static/media/meme_3.a2357457.png"
+  },
+  {
+    "revision": "6ea0260adc124e38464df3342ab38baf",
+    "url": "/Memes_Ranking/static/media/meme_4.6ea0260a.png"
+  },
+  {
+    "revision": "5ad854138f98b085c5e8df0489040564",
+    "url": "/Memes_Ranking/static/media/meme_5.5ad85413.jpg"
+  },
+  {
+    "revision": "ad8a4322054c4fb767c7f17e3ec380f7",
+    "url": "/Memes_Ranking/static/media/meme_6.ad8a4322.png"
+  },
+  {
+    "revision": "8facd1c90b4c3412db418b142f714c45",
+    "url": "/Memes_Ranking/static/media/meme_7.8facd1c9.jpg"
+  },
+  {
+    "revision": "5e6c0a83f26b0e958c23e5987e5fafbb",
+    "url": "/Memes_Ranking/static/media/meme_8.5e6c0a83.jpg"
+  },
+  {
+    "revision": "d18cfc8613aa024b72bc2d2ea10ca124",
+    "url": "/Memes_Ranking/static/media/meme_9.d18cfc86.png"
+  },
+  {
+    "revision": "ec788ed5863673ed46af86d8b5804f62",
+    "url": "/Memes_Ranking/static/media/meme_10.ec788ed5.jpg"
+  },
+  {
+    "revision": "fbea1c70a5a9fb8363205cb1ffcfa8b8",
+    "url": "/Memes_Ranking/static/media/meme_11.fbea1c70.jpg"
+  },
+  {
+    "revision": "f207e0b62df9ff1d950b2c0bd6476163",
+    "url": "/Memes_Ranking/static/media/meme_12.f207e0b6.jpg"
+  },
+  {
+    "revision": "708ea31d8b930a9c409f87140f73258a",
+    "url": "/Memes_Ranking/static/media/meme_13.708ea31d.png"
+  },
+  {
+    "revision": "7c1e29b84225264520650328c198074b",
+    "url": "/Memes_Ranking/static/media/meme_14.7c1e29b8.jpg"
+  },
+  {
+    "revision": "f430838932d4adffae08dc7e67c2e606",
+    "url": "/Memes_Ranking/static/media/meme_15.f4308389.jpg"
+  },
+  {
+    "revision": "1029da2de2dc7eea25e8d4eb42d05347",
+    "url": "/Memes_Ranking/static/media/meme_16.1029da2d.png"
+  },
+  {
+    "revision": "a58e5a17116ec59c266c4aa7c623de5c",
+    "url": "/Memes_Ranking/index.html"
+  }
+];
